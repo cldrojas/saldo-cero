c8b11c4dd0485cc617fb3ca5662cd960f5e36e58
cldrojas <cld.rojasz@gmail.com>
2025-06-23 01:50:33 -0400

    refactor: enhance type definitions for Movement and Account

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,5 +1,34 @@
-type Runtime = import("@astrojs/cloudflare").Runtime<Env>;
+type Runtime = import('@astrojs/cloudflare').Runtime<Env>
 
 declare namespace App {
-	interface Locals extends Runtime {}
+  interface Locals extends Runtime {}
 }
+
+export type Movement = {
+  id: `${string}-${string}-${string}-${string}-${string}`
+  description: string
+  account: Account
+  amount: number
+  type: 'income' | 'expense' | 'transfer'
+  date: Date
+}
+
+export type Expense = Movement & {
+  type: 'expense'
+}
+export type Income = Movement & {
+  type: 'income'
+}
+export type Transfer = Movement & {
+  type: 'transfer'
+}
+
+export type Account = {
+  id: `${string}-${string}-${string}-${string}-${string}`
+  name: string
+  balance: number
+}
+// export type Env = {
+// 	MOVEMENTS: KVNamespace
+// 	ACCOUNTS: KVNamespace
+// }
